57bb627f177bb14a12e05009768485a344f1e147
David <david.lopes@akross.com.br>
2024-10-08 15:12:32 -0300

    refactor: change error message

--- internal/domain/usecase/account/get_account_by_id.go
+++ internal/domain/usecase/account/get_account_by_id.go
@@ -9,7 +9,7 @@ import (
 func (a Account) GetAccountById(ctx context.Context, accountId string) (*entities.Account, error) {
 	acc, err := a.repository.GetAccountById(ctx, accountId)
 	if err != nil {
-		return nil, fmt.Errorf("GetAccountById error: %w", err)
+		return nil, fmt.Errorf("get account by id: %w", err)
 	}
 	return acc, nil
 }
