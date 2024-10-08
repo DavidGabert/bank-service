a8099728e1d72968b2d1f43febc29e60e7f8623a
David <david.lopes@akross.com.br>
2024-10-08 14:14:48 -0300

    feat: create getAccountById method on interface

--- internal/domain/usecase/account/usecase.go
+++ internal/domain/usecase/account/usecase.go
@@ -7,4 +7,5 @@ import (
 
 type UseCase interface {
 	Create(ctx context.Context, input entities.CreateAccountInput) (*entities.Account, error)
+	GetAccountById(ctx context.Context, accountId string) (*entities.Account, error)
 }
