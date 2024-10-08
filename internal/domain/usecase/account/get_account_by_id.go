1568164ebc797be4eef760faaa2093b3863d10f5
David <david.lopes@akross.com.br>
2024-10-08 14:16:33 -0300

    feat: create getAccountById useCase and tests

---
+++ internal/domain/usecase/account/get_account_by_id.go
@@ -0,0 +1,15 @@
+package account
+
+import (
+	"bank-service/internal/domain/entities"
+	"context"
+	"fmt"
+)
+
+func (a Account) GetAccountById(ctx context.Context, accountId string) (*entities.Account, error) {
+	acc, err := a.repository.GetAccountById(ctx, accountId)
+	if err != nil {
+		return nil, fmt.Errorf("GetAccountBalance error: %w", err)
+	}
+	return acc, nil
+}
