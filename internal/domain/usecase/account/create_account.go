bc106911e468c7847ad2c9d51ced9bbc8e49f9b9
David <david.lopes@akross.com.br>
2024-10-09 12:36:15 -0300

    refactor: change create account useCase

--- internal/domain/usecase/account/create_account.go
+++ internal/domain/usecase/account/create_account.go
@@ -1,13 +1,26 @@
 package account
 
 import (
+	"bank-service/internal/domain/common/hash"
 	"bank-service/internal/domain/entities"
 	"context"
 	"fmt"
 )
 
+var (
+	ErrCPFAlreadyLinked = fmt.Errorf("CPF is already linked to an account")
+)
+
 func (a Account) Create(ctx context.Context, input entities.CreateAccountInput) (*entities.Account, error) {
-	acc, err := a.repository.Create(ctx, entities.NewAccount(input.Name, input.CPF, input.Secret))
+	acc, err := a.repository.GetAccountByCpf(ctx, input.CPF)
+	if err != nil {
+		return nil, fmt.Errorf("get account by cpf: %w", err)
+	}
+	if acc != nil {
+		return nil, ErrCPFAlreadyLinked
+	}
+	hashSecret := hash.Hash(input.Secret)
+	acc, err = a.repository.Create(ctx, entities.NewAccount(input.Name, input.CPF, hashSecret))
 	if err != nil {
 		return nil, fmt.Errorf("error creating account: %w", err)
 	}
