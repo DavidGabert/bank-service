adda9d66d8ba91a3493514b604792c418bf6c8d9
David <david.lopes@akross.com.br>
2024-10-17 14:40:30 -0300

    feat: Add account routes

--- internal/gateways/http/account/handler.go
+++ internal/gateways/http/account/handler.go
@@ -1,9 +1,17 @@
 package account
 
 import (
+	"bank-service/extension/http/rest"
 	"bank-service/internal/domain/usecase/account"
+	"github.com/go-chi/chi/v5"
 )
 
 type Handler struct {
 	AccountUc account.UseCase
+}
+
+func (h Handler) AccountRoutes(r chi.Router) {
+	r.Post("/accounts", rest.Handle(h.Create))
+	r.Get("/accounts", rest.Handle(h.GetBalance))
+	r.Get("/accounts/{account-id}/balance", rest.Handle(h.GetBalance))
 }
